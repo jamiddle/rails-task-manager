6e28e973fd5b4f0159900e8810b05406275b264e
James Middlemass <james.middlemass01@gmail.com>
2019-11-12 14:30:40 +0000

    finished task manager with rails

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
   end
 
   def create
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def index
@@ -19,16 +22,19 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
-  def new_params
+  def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Create
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: 'new'
   post 'tasks', to: 'tasks#create'
 
   # Read
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   get 'tasks/:id', to: 'tasks#show', as: 'task'
 
   # Update
-  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
   patch 'tasks/:id', to: 'tasks#update'
 
   # Delete
-  delete 'tasks/:id', to: 'task#destroy'
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete'
 end
